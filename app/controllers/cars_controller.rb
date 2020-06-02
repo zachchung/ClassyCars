129879207ee32be428e4cec3b26e268608718105
harshilsiyani <siyaniharsh@gmail.com>
2020-06-02 16:51:31 +1000

    ADDED: create new car and show the car

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -6,12 +6,17 @@ class CarsController < ApplicationController
 
   def create
     @car = Car.new(car_params)
+    @car.user = current_user
 
     if @car.save
       redirect_to car_path(@car)
     else
+
       render :new
     end
+  end
+  def show
+    @car = Car.find(params[:id])
   end
 
   def index
