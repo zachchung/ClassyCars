62e3de0ce9b23f7c921e426032b257860b39efc3
Erik Tran <erik.tran.tt@gmail.com>
2020-06-02 14:40:07 +1000

    add gem "faker", generated seeds for User, Car and Booking

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+suburbs = %w[Brunswick Melbourne South\ Yarra Docklands South\ Melbourne Footscray];
+booking_status = %w[confirmed cancelled renting]
+
+# Sample Users
+user = User.new(
+  first_name: Faker::Name.unique.first_name,
+  last_name: Faker::Name.unique.last_name,
+  email: "simon.smith@cars.com",
+  address: "Melbourne, AUS"
+)
+user.password = "abc123"
+user.save!
+
+# Sample Cars
+10.times do
+  car = Car.new(
+    name: "#{Faker::Vehicle.manufacture} #{Faker::Vehicle.make_and_model}",
+    year: rand(1900..1980),
+    seats: rand(2..6),
+    price: rand(50..100) + rand.floor(2),
+    location: "#{suburbs.sample}, VIC, AUS"
+  )
+  car.user = user if rand(1..5) == 3
+  car.save!
+end
+
+# Sample Booking
+5.times do 
+  booking = Booking.new(
+    start_date: DateTime.now,
+    end_date: DateTime.now + rand(1..7)
+  )
+  booking.user = user
+  booking.car = Car.find(rand(Car.first.id..Car.last.id))
+  booking.booking_price = (booking.end_date - booking.start_date) * booking.car.price
+  booking.status = booking_status.sample
+  booking.save!
+end
+
+# Sample Reviews -- placeholder
