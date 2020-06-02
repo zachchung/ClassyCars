8ac8be91a96f7b4c2f6e461dad1a7788193e0162
zach chung <chungchunluk@gmail.com>
2020-06-02 14:42:36 +0800

    booking confirmation page done, wip: booking index

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,32 +1,37 @@
-require 'date'
-
 class BookingsController < ApplicationController
   # confirm page:
   def new
-    # GET FROM PARAMS
-    @car = Car.new(name: "Porsche 911", price: 500, location: "Melbourne")
-    @start_date = Date.parse('28-12-2010')
-    @end_date = Date.parse('31-12-2010')
+    # GET FROM CARS#SHOW'S PARAMS
+    # @car = Car.new(name: "Porsche 911", price: 500, location: "Melbourne")
+    # @start_date = Date.parse('28-12-2020')
+    # @end_date = Date.parse('31-12-2020')
+    @car = Car.find(rand(1..10))
+    @start_date = DateTime.now
+    @end_date = DateTime.now + rand(1..7)
+
     # END
 
     @days = (@end_date - @start_date).to_i
-    @booking_price = @car.price * @days
+    @booking_price = (@car.price * @days).round(2)
     # @booking = Booking.new
   end
 
-  private
-
-  def create
-    @booking = Booking.new(booking_params)
-    if @booking.save
-      redirect_to @booking
-    else
-      render :new
-    end
+  def index
+    @bookings = Booking.where(user: current_user)
   end
 
+  # private
 
-  def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :booking_price)
-  end
+  # def create
+  #   @booking = Booking.new(booking_params)
+  #   if @booking.save
+  #     redirect_to @booking
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  # def booking_params
+  #   params.require(:booking).permit(:start_date, :end_date, :booking_price)
+  # end
 end
