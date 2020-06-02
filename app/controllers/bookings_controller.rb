63ed04f363ef0985490dfc0660cf496dce2d6774
harshilsiyani <siyaniharsh@gmail.com>
2020-06-03 07:34:15 +1000

    Booking form passes the hidden params of car.id but not user.id

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -44,6 +44,6 @@ class BookingsController < ApplicationController
   # end
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date)
+    params.require(:booking).permit(:start_date, :end_date, :car_id, :user_id)
   end
 end
