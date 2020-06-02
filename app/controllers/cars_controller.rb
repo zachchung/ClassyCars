3dc4c686921648caf957c6eb7318e26aab67cdb2
harshilsiyani <siyaniharsh@gmail.com>
2020-06-02 14:32:29 +1000

    New and Create done

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,15 @@
 class CarsController < ApplicationController
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new
+
+    if @car.save
+      redirect_to car_path(@car)
+    else
+      render :new
+    end
+  end
 end
