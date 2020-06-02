1c0e748e930954c85230afc01af81ce5d216bd45
zach chung <chungchunluk@gmail.com>
2020-06-02 18:04:09 +0800

    wip

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,13 +2,9 @@ class BookingsController < ApplicationController
   # confirm page:
   def new
     # GET FROM CARS#SHOW'S PARAMS
-    # @car = Car.new(name: "Porsche 911", price: 500, location: "Melbourne")
-    # @start_date = Date.parse('28-12-2020')
-    # @end_date = Date.parse('31-12-2020')
     @car = Car.find(rand(1..10))
     @start_date = DateTime.now
     @end_date = DateTime.now + rand(1..7)
-
     # END
 
     @days = (@end_date - @start_date).to_i
@@ -32,16 +28,7 @@ class BookingsController < ApplicationController
   def show
   end
 
-  # private
-
-  # def create
-  #   @booking = Booking.new(booking_params)
-  #   if @booking.save
-  #     redirect_to @booking
-  #   else
-  #     render :new
-  #   end
-  # end
+  private
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
