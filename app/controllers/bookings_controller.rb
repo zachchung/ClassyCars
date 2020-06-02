2c2fb32b64d778d63588c181f3f308f7c03af094
zach chung <chungchunluk@gmail.com>
2020-06-02 12:57:31 +0800

    wip- booking#new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,30 @@
+require 'date'
+
 class BookingsController < ApplicationController
+  def new
+    # BEING PASSED
+    @car = Car.new(name: "Porsche 911", price: 500, location: "Melbourne")
+    @start_date = Date.parse('28-12-2010')
+    @end_date = Date.parse('31-12-2010')
+    # END
+
+    @days = (@end_date - @start_date).to_i
+    @booking = Booking.new
+    @booking_price = @car.price * @days
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :booking_price)
+  end
 end
