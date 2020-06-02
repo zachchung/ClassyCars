16966cdec249ddfe69d55a79acbac93961ebd5fa
harshilsiyani <siyaniharsh@gmail.com>
2020-06-02 15:24:12 +1000

    Create a car done

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,15 +1,26 @@
 class CarsController < ApplicationController
+
   def new
     @car = Car.new
   end
 
   def create
-    @car = Car.new
+    @car = Car.new(car_params)
 
     if @car.save
       redirect_to car_path(@car)
     else
       render :new
     end
+  end
+
+  def index
+    @cars = Car.all
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:name, :location, :seats, :year, :price)
   end
 end
