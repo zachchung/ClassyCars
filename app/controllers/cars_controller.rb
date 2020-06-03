f6f1eebaee5afe8da278dcce1ffa09fcbb25f268
harshilsiyani <siyaniharsh@gmail.com>
2020-06-03 13:10:17 +1000

    IN-PROGRESS: working on displaying owners car

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -30,8 +30,19 @@ class CarsController < ApplicationController
     @car.destroy
 
     # no need for app/views/restaurants/destroy.html.erb
-    redirect_to car_path
+    redirect_to cars_path
   end
+
+  def update
+    @car = Car.find(params[:id])
+    @car.update(params[:car])
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+  # def listmycars
+  #   current_user.car_ids.each do |id|
+  #     @car = Car.find(id)
+  #   end
+
 
   private
 
