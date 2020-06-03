246bebce8605afec871c3141274f03d642d998e6
Erik Tran <erik.tran.tt@gmail.com>
2020-06-03 19:13:20 +1000

    CHANGED: Change user's first and last names to match real world, and let other users make bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,17 +16,17 @@ user = User.create!(
   )
 
 second_user = User.new(
-  first_name: Faker::Name.unique.first_name,
-  last_name: Faker::Name.unique.last_name,
+  first_name: "Erik",
+  last_name: "Tran",
   email: "erik@cars.com",
-  address: "Melbourne, AUS",
+  address: "Brunswick, Melbourne, AUS",
 )
 second_user.password = "abc123"
 second_user.save!
 
 third_user = User.new(
-  first_name: Faker::Name.unique.first_name,
-  last_name: Faker::Name.unique.last_name,
+  first_name: "Harshil",
+  last_name: "Siyani",
   email: "harshil@cars.com",
   address: "Melbourne, AUS",
 )
@@ -56,10 +56,10 @@ puts "Finished generated Car ---"
 puts "--- Generate Booking"
 5.times do
   booking = Booking.new(
-    start_date: DateTime.now,
-    end_date: DateTime.now + rand(1..7)
+    start_date: Date.today,
+    end_date: Date.today + rand(1..7)
   )
-  booking.user = user
+  booking.user = users.sample
   booking.car = Car.find(rand(Car.first.id..Car.last.id))
   booking.booking_price = (booking.end_date - booking.start_date) * booking.car.price
   booking.status = booking_status.sample
