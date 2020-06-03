559e68f67baed98bc8e3ff3c53e0a87b9dc3e813
Erik Tran <erik.tran.tt@gmail.com>
2020-06-03 12:12:36 +1000

    FIXED: correctly passed user_id from booking form to bookings_controller#create and display on confirmation page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,23 +1,11 @@
 class BookingsController < ApplicationController
-  # confirm page:
-  def new
-    # GET FROM CARS#SHOW'S PARAMS
-    # @car = Car.new(name: "Porsche 911", price: 500, location: "Melbourne")
-    # @start_date = Date.parse('28-12-2020')
-    # @end_date = Date.parse('31-12-2020')
-    @car = Car.find(rand(1..10))
-    @start_date = DateTime.now
-    @end_date = DateTime.now + rand(1..7)
+  booking_status = %w[pending confirmed cancelled returned renting]
 
-    # END
-
-    @days = (@end_date - @start_date).to_i
-    @booking_price = (@car.price * @days).round(2)
-    # @booking = Booking.new
-  end
+  def new; end
 
   def create
     @booking = Booking.new(booking_params)
+    @booking.status = "pending"
     if @booking.save
       redirect_to @booking
     else
@@ -33,16 +21,7 @@ class BookingsController < ApplicationController
     @booking = Booking.find(params[:id])
   end
 
-  # private
-
-  # def create
-  #   @booking = Booking.new(booking_params)
-  #   if @booking.save
-  #     redirect_to @booking
-  #   else
-  #     render :new
-  #   end
-  # end
+  private
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date, :car_id, :user_id)
