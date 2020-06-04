8b36a32b71ad1f933d8b5d79d70e4506f252833f
harshilsiyani <siyaniharsh@gmail.com>
2020-06-04 17:35:08 +1000

    FEAT: car is being able to update

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -50,11 +50,17 @@ class CarsController < ApplicationController
     end
   end
 
-  # def edit
-  #   @car = Car.find(params[:id])
-  #   @car.update(car_params)
-  #   # Will raise ActiveModel::ForbiddenAttributesError
-  # end
+  def edit
+    @car = Car.find(params[:id])
+
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    @car.update(car_params)
+    redirect_to listmycars_cars_path
+  end
 
   def listmycars
     @cars = current_user.cars
