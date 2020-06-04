3cf2e922bf6a28feeb8362e1db53ee0791efb8df
harshilsiyani <siyaniharsh@gmail.com>
2020-06-04 16:58:36 +1000

    home page background stylized

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    if params[:query].present?
+      @cars = Car.where("location ILIKE ?", "%#{params[:query]}%")
+    else
+      @cars = Car.all
+    end
   end
+
 end
