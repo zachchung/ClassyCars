4e1175739a6a5d3adedb56d168fa07e130599edf
Erik Tran <erik.tran.tt@gmail.com>
2020-06-05 15:24:07 +1000

    FIXED: map display

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -4,7 +4,7 @@ import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 5000 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 14, duration: 3000 });
 };
 
 const addMarkersToMap = (map, markers) => {
@@ -40,8 +40,16 @@ const initMapbox = () => {
     map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
                                       mapboxgl: mapboxgl }));
 
-    fitMapToMarkers(map, markers);
-    addMarkersToMap(map, markers);
+    
+    if (Object.keys(markers).length > 0) {
+      addMarkersToMap(map, markers);
+      map.flyTo({
+        center: [markers[0].lng, markers[0].lat],
+        zoom: 13,
+        maxZoom: 15,
+      });
+      fitMapToMarkers(map, markers);
+    }
   }
 };
 
