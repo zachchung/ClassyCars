bc15038d376945c974babd997ddb4423f521d593
Erik Tran <erik.tran.tt@gmail.com>
2020-06-05 15:30:35 +1000

    MERGED conflict init_mapbox

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -4,7 +4,7 @@ import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 14, duration: 3000 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 3000 });
 };
 
 const addMarkersToMap = (map, markers) => {
