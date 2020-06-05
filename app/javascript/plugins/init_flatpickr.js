e3ad6e7078b94271f9a928d0c681591fa5ed8a0b
Erik Tran <erik.tran.tt@gmail.com>
2020-06-05 11:48:02 +1000

    FIXED styling

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -6,8 +6,6 @@ import flatpickr from "flatpickr";
 const initFlatpickr = () => {
   const startDateInput = document.getElementById('booking_start_date');
   const endDateInput = document.getElementById('booking_end_date');
-  console.log(startDateInput);
-  console.log(endDateInput);
   
   // Check that the query selector id matches the one you put around your form.
   if (startDateInput) {
